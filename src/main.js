d8cc14a4090b52e8db4b23105efb661df076e1d4
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2019-01-22 01:26:07 +0100

    Perfect zoom

--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
 
 function setup(){
-    let svg = d3.select("body")
+    let svg = d3.select("#map")
         .append("svg")
         .attr("width",WIDTH)
         .attr("height",HEIGHT);
+
+    let loadingText = svg
+        .append("text")
+        .attr("x","50%")
+        .attr("y","50%")
+        .attr("text-anchor","middle")
+        .text("Cargando... 0%");
 
     let mapData = d3.json("/data/Castile and León_AL8.GeoJson");
     let herbaceosData = d3.csv("/data/herbaceos_2017.csv");
@@ -16,24 +23,51 @@ function setup(){
     loading.forEach(p => {
         p.then(()=>{
             count++;
-            console.log(`Progress: ${count/loading.length}`);
+            loadingText.text(`Cargando... ${Math.floor(100*count/loading.length)}%`);
         });
     })
     Promise.all(loading).then(data => {
         let mapData = data[0];
         let herbaceosData = data[1];
         let leñososData = data[2];
+        loadingText.remove();
         main(svg,mapData,herbaceosData,leñososData);
-        console.log("Finished loading");
     });
 }
 
 function main(svg,mapData,herbaceosData,leñososData){
     let features = mapData.features;
     let projection = d3.geoMercator()
-        .fitExtent([[0,40],[WIDTH,HEIGHT]],mapData);
+        .fitExtent([[0,50],[WIDTH,HEIGHT-50]],mapData);
     let geoPath = d3.geoPath(projection);
-    svg.selectAll(".country")
+
+
+
+
+    let zoom = d3.zoom().on("zoom",zoomed);
+    let map = svg
+        .append("g")
+        .call(zoom);
+    
+    
+
+    function zoomed(){
+        let transform = d3.event.transform;
+        map.selectAll(".country").attr("transform",transform.toString());
+    }
+
+    let invisibleRect = map
+    .append("rect");
+
+    invisibleRect
+        .attr("x",0)
+        .attr("y",0)
+        .attr("width",WIDTH)
+        .attr("height",HEIGHT)
+        .style("fill","red")
+        .style("opacity",0);
+        
+    map.selectAll(".country")
         .data(features)
         .enter()
         .append("path")
@@ -43,6 +77,13 @@ function main(svg,mapData,herbaceosData,leñososData){
         .style("fill",(data,i)=>{
             let colors = ["black"];
             return colors[i%colors.length];
+        })
+        .on("mouseover",(data)=>{
+
+        })
+        .on("click",(data)=>{
+            if (d3.event.defaultPrevented) return;
+            alert("Municipio: "+data.properties.localname);
         });
 }
 
